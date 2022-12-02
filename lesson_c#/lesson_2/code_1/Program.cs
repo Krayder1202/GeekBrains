ee69cafad83175e036dd25c3699a466ebe607778
Krayder1202 <rjnekitos@gmail.com>
2022-12-02 19:53:05 +0300

    добавил задачи из лекции 2

---
+++ lesson_c#/lesson_2/code_1/Program.cs
@@ -0,0 +1,27 @@
+﻿int a1= 56;
+int b1= 75;
+int c1= 37;
+int a2= 87;
+int b2= 54;
+int c2= 65;
+int a3= 23;
+int b3= 57;
+int c3= 82;
+
+int max1= Max (a1,b1,c1);
+int max2= Max (a2,b2,c2);
+int max3= Max (a3,b3,c3);
+int max= Max (max1,max2,max3);
+
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2>result) result = arg2;
+    if (arg3>result) result = arg3;
+    return result;
+}
+Console.WriteLine(max1);
+Console.WriteLine(max2);
+Console.WriteLine(max3);
+Console.WriteLine(max);
---
+++ lesson_c#/lesson_2/code_3/Program.cs
@@ -0,0 +1,17 @@
+﻿//             0   1   2   3   4   5   6   7   8   9           
+int [] array = {16, 47, 64, 35, 36, 28, 55, 35, 49, 58};
+
+int n = array.Length;
+int find = 35;
+
+int index = 0;
+
+while (index<n)
+{
+    if (array[index]==find)
+    {
+    Console.WriteLine(index);
+    break;
+    }
+    index++;
+}
