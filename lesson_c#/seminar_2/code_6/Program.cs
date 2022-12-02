4594b84f6c877689370d76dd4d93efd74d6d4737
Krayder1202 <rjnekitos@gmail.com>
2022-12-02 22:16:12 +0300

    Исправление ошибок в задачах

---
+++ lesson_c#/seminar_2/code_6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Input first number");
+int firstNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Input second number");
+int secondNumber = int.Parse(Console.ReadLine());
+
+if(firstNumber == Math.Pow(secondNumber, 2) || secondNumber == Math.Pow(firstNumber, 2))
+{
+System.Console.WriteLine("Profit");
+}
+else
+{
+    System.Console.WriteLine("No Profit");
+}
