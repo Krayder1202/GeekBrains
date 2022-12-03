eb0b57737b562d2e48428c9af093bd1f9ce89a39
Krayder1202 <rjnekitos@gmail.com>
2022-12-03 20:51:10 +0300

    все задачи со второго семинара добавлены

---
+++ lesson_c#/seminar_1/code_5/Program.cs
@@ -0,0 +1,15 @@
+﻿// Домашка
+
+Console.WriteLine("Input first number");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Input second number");
+int b = int.Parse(Console.ReadLine());
+if (a>b)
+{
+    System.Console.WriteLine(a);
+}
+else 
+{
+    System.Console.WriteLine(b);
+}
