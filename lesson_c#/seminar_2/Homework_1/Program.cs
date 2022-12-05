809caedda59fe45eefd98ce76345c1e2b889806c
Krayder1202 <rjnekitos@gmail.com>
2022-12-05 13:39:46 +0300

    Добавил ДЗ ко второму семинару по C#

---
+++ lesson_c#/seminar_2/Homework_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Домашка 1
+
+Console.WriteLine("Input number");
+int number = int.Parse(Console.ReadLine());
+
+int firstPart = number % 100;
+int secondPart = firstPart / 10;
+
+
+System.Console.WriteLine(secondPart);
+
---
+++ lesson_c#/seminar_2/Homework_3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Домашка 3
+
+Console.WriteLine("input number?"); //вывод на экран сообщения//
+int number=int.Parse(Console.ReadLine()); //ввод в терминал первого значения//
+
+switch (number)
+{
+    case 1:
+        number = 1;
+        Console.WriteLine("no"); //вывод на экран сообщения//
+        break;
+    case 2:
+        number = 2;
+        Console.WriteLine("no");//вывод на экран сообщения//
+        break;
+    case 3:
+        number = 3;
+        Console.WriteLine("no"); //вывод на экран сообщения//
+        break;
+    case 4:
+    number = 4;
+        Console.WriteLine("no"); //вывод на экран сообщения//
+        break;
+    case 5:
+        number = 5;
+        Console.WriteLine("no"); //вывод на экран сообщения//
+        break;
+    case 6:
+        number = 6;
+        Console.WriteLine("yes"); //вывод на экран сообщения//
+        break;
+    case 7:
+        number = 7;
+        Console.WriteLine("yes"); //вывод на экран сообщения//
+        break;
+}
