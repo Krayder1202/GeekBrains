91865a01c68ba4eb105d8dd0b5d1785d6de347fa
Krayder1202 <rjnekitos@gmail.com>
2022-12-07 21:08:00 +0300

    Добавил домашнее задание к третьему семинару по C#

--- lesson_c#/seminar_3/Homework_1/Program.cs
+++ lesson_c#/seminar_3/Homework_1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Домашка 1
+
+Console.WriteLine("Input number");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine("Profit");
+  }
+  else Console.WriteLine("No Profit");
+}
+
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine("error");
--- lesson_c#/seminar_3/Homework_2/Program.cs
+++ lesson_c#/seminar_3/Homework_2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Домашка 2
+
+Console.WriteLine("input number?");
+
+double ax = double.Parse(Console.ReadLine()!);
+double ay = double.Parse(Console.ReadLine()!);
+double az = double.Parse(Console.ReadLine()!);
+double bx = double.Parse(Console.ReadLine()!);
+double by = double.Parse(Console.ReadLine()!);
+double bz = double.Parse(Console.ReadLine()!);
+
+double ab = Math.Pow ( bx- ax, 2) + Math.Pow (by - ay, 2) + Math.Pow (bz - az, 2); // возведение в степень
+
+ab = Math.Sqrt (ab); //вычесление квадратного корня
+ab = Math.Round (ab, 3); //округление
+System.Console.WriteLine($"input number {ab}");
