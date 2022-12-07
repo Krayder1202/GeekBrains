8a5380f26b92c3045ba773addfd674dbfc59116d
Krayder1202 <rjnekitos@gmail.com>
2022-12-07 22:10:28 +0300

    добавил часть дз к четверному семинару по C#

---
+++ lesson_c#/seminar_4/Homework_1/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Домашка 1
+
+Console.WriteLine(Calculate(3, 5));
+int Calculate (int firstNumber, int secondNumber)
+
+{
+  int result = 1;
+  for(int i=1; i <= secondNumber; i++)
+  {
+    result = result * firstNumber;
+  }
+   
+    return result;
+}
---
+++ lesson_c#/seminar_4/Homework_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Домашка 2
+
+//Console.WriteLine("Input number");
+//int number = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int number)
+  {
+    
+    int x = Convert.ToString(number).Length;
+    int y = 0;
+    int result = 0;
+
+    for (int i = 0; i < x; i++){
+      y = number - number % 10;
+      result = result + (number - y);
+      number = number / 10;
+    }
+   return result;
+  }
+int sumNumber = SumNumber(number);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
