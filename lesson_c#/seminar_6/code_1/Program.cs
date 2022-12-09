e854704714dbf90b8c2e4dfe3e79c0977cbc678f
Krayder1202 <rjnekitos@gmail.com>
2022-12-09 22:56:31 +0300

    Добавил задния к шестому семинару по с#

---
+++ lesson_c#/seminar_6/code_1/Program.cs
@@ -0,0 +1,43 @@
+﻿int Revers(int [] array1)
+	{
+		for (int i = 0; i < array1.Length/2; i++)
+		{
+			ChengeElements(array1, i, array1.Length-1-i);
+		}
+	}
+
+int [] Array(int length)
+	{
+		int [] array = new int[length];
+		for (int i = 0; i < length; i++)
+		{
+		{
+			array [i] =i;
+		}
+		return array;
+		}
+	}
+
+string GetArray(int[] array)
+	{
+		string result = string.Empty;
+		for (int i = 0; i < array.Length; i++)
+		{
+			result += $"{array[i]}  ";
+		}
+		return result;
+	}
+
+void ChengeElements(int [] array, int index1, int indexL)
+	{
+		int temp = array[index1]; 
+		array[index1] = array[indexL]; 
+		array[indexL] = temp;
+	}
+
+
+int a = Array(10);
+System.Console.WriteLine(GetArray(a));
+
+Revers(a);
+System.Console.WriteLine(GetArray(a));
---
+++ lesson_c#/seminar_6/code_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// 
+
+Console.WriteLine("Input number");
+int number = int.Parse(Console.ReadLine());
+string a = string.Empty;
+while (number>0)
+{
+    a+= number%2;
+    number =number/2;
+}
+System.Console.WriteLine(a);
