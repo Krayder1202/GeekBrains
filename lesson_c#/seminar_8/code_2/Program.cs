ff70a515c537308b5819f7789a4d3fa9d35df9b2
Krayder1202 <rjnekitos@gmail.com>
2022-12-14 20:28:40 +0300

    Добавил домашнее задание к 8 семинару по C#

--- lesson_c#/seminar_8/code_2/Program.cs
+++ lesson_c#/seminar_8/code_2/Program.cs
@@ -1,35 +1,4 @@
-﻿int[,] inputTwoDimenstions(int length, int secondLength)
-{
-    int[,] mass = new int[length, secondLength];
-    for (int i = 0; i < mass.GetLength(0); i++)
-    {
-        FillDimenstion(i, mass);
-    }
-    return mass;
-}
-
-int ReadInt()
-{
-    Console.WriteLine("Input number");
-    int a = int.Parse(Console.ReadLine());
-    return a;
-}
-
-void FillDimenstion(int index, int[,] massiv)
-{
-    for (int i = 0; i < massiv.GetLength(1); i++)
-    {
-        massiv[index, i] = new Random().Next(1, 10);
-    }
-}
-
-void GetArrayAsString(int[,] massive)
-{
-    for (int i = 0; i < massive.GetLength(0); i++)
-    {
-        for (int j = 0; j < massive.GetLength(1); j++)
-        {
-            Console.Write($"{massive[i, j]} ");
+﻿i, j]} ");
         }
         Console.WriteLine();
     }
