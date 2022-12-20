1860d0a37ef95e8a019fa3e1dd3178c8a7089ba7
Krayder1202 <rjnekitos@gmail.com>
2022-12-20 20:01:59 +0300

    добавил итоговый проект

---
+++ lesson_c#/seminar_9/code_1/Program.cs
@@ -0,0 +1,31 @@
+﻿string MetodFor(int a, int b)
+{
+	string result = String.Empty;
+	for (int i = a; i <= b; i++)
+	{
+		result += $"{i} ";
+	}
+	return result;
+}
+Console.WriteLine(MetodFor(1, 10));
+
+//Метод с рекурсией
+string MetodRecursion (int a, int b)
+{
+	if (a <= b) return $"{a} " + MetodRecursion(a + 1, b);
+	else return string.Empty;
+}
+Console.WriteLine(MetodRecursion(ReadInt(), ReadInt()));
+
+int ReadInt()
+{
+    Console.WriteLine("Введите число: ");
+    if (int.TryParse(Console.ReadLine(), out int i))
+        return i;
+    else
+    {
+        Console.WriteLine("Это не число: ");
+        return -1;
+    }
+
+}
